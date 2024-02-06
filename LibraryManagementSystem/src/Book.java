a11d274b6c5b5d2813f76e0b39661bd97fe92835
methminimadhush2000 <130038736+methminimadhush2000@users.noreply.github.com>
2024-02-06 11:36:16 +0530

    Add files via upload

---
+++ LibraryManagementSystem/src/Book.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+class Book {
+    private String title;
+
+    private  String author;
+    private int number;
+    private  int price;
+    private  int noOfCopies;
+
+
+
+    public Book(String title, String author,  int number,int price, int noOfCopies){
+        this.title=title;
+        this.author=author;
+        this.number=number;
+        this.price=price;
+        this.noOfCopies=noOfCopies;
+    }
+    public String getTitle(){
+
+        return title;
+    }
+    public String getAuthor(){
+
+        return author;
+    }
+    public void setTitle(){
+
+    }
+
+
+
+}
---
+++ LibraryManagementSystem/src/User.java
@@ -0,0 +1,19 @@
+public class User {
+    String name;
+    int userId;
+
+    public User(String name, int userId) {
+        this.name = name;
+        this.userId = userId;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+}
